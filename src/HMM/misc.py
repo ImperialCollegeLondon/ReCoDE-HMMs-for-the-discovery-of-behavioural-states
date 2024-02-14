78d2377b8b4bd3decee7480a19057f6bef121d40
Lblackhurst29 <l.blackhurst19@imperial.ac.uk>
2024-02-14 15:31:09 +0000

    updated notebooks

--- src/HMM/misc.py
+++ src/HMM/misc.py
@@ -2,6 +2,7 @@ from itertools import cycle
 import numpy as np 
 from math import floor, ceil
 import pandas as pd
+from pathlib import PurePath
 
 def rle(x):
     """
@@ -38,24 +39,59 @@ def rle(x):
 
         return run_values, run_starts, run_lengths
 
-def bin_data(data, column, bin_column, function, bin_secs):
-    """ a method that will bin all data poits to a larger time bin and then summarise a column """
-    index_name = data['id'].iloc[0]
+def create_chain(states, start, length):
 
-    data[bin_column] = data[bin_column].map(lambda t: bin_secs * floor(t / bin_secs))
-    output_parse_name = f'{column}_{function}' # create new column name
+    if any([name == start for name in states]) is False:
+        raise TypeError('The starting state name is not in the states list')
 
-    bout_gb = data.groupby(bin_column).agg(**{
-        output_parse_name : (column, function)    
-    })
+    mat_trans =   [[0.7, 0.05, 0.25],
+                    [0.35, 0.5, 0.15],
+                    [0.05, 0.3, 0.65]]
 
-    bin_parse_name = f'{bin_column}_bin'
-    bout_gb.rename_axis(bin_parse_name, inplace = True)
-    bout_gb.reset_index(level=0, inplace=True)
-    old_index = pd.Index([index_name] * len(bout_gb.index), name = 'id')
-    bout_gb.set_index(old_index, inplace =True)
+    i = 0
 
-    return bout_gb
+    markov_chain = [start]
+    mood = start
+
+    while i != length:
+
+        for c, state in enumerate(states):
+
+            if mood == state:
+                mood = np.random.choice(states,replace=True,p=mat_trans[c])
+                markov_chain.append(mood)
+                break
+
+        i += 1
+    
+    return markov_chain
+
+def get_data():
+    path = PurePath(__file__)
+    this_dir = path.parent
+    parent_dir = str(this_dir).replace("notebooks/../src/HMM", "")
+    file = PurePath(parent_dir) / "data/training_data_small.zip"
+    return pd.read_csv(file)
+
+
+# def bin_data(data, column, bin_column, function, bin_secs):
+#     """ a method that will bin all data poits to a larger time bin and then summarise a column """
+#     index_name = data['id'].iloc[0]
+
+#     data[bin_column] = data[bin_column].map(lambda t: bin_secs * floor(t / bin_secs))
+#     output_parse_name = f'{column}_{function}' # create new column name
+
+#     bout_gb = data.groupby(bin_column).agg(**{
+#         output_parse_name : (column, function)    
+#     })
+
+#     bin_parse_name = f'{bin_column}_bin'
+#     bout_gb.rename_axis(bin_parse_name, inplace = True)
+#     bout_gb.reset_index(level=0, inplace=True)
+#     old_index = pd.Index([index_name] * len(bout_gb.index), name = 'id')
+#     bout_gb.set_index(old_index, inplace =True)
+
+#     return bout_gb
 
 def bootstrap(data, n=1000, func=np.mean):
     """ 
