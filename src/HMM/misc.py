5ed7eb792564b2a72f0aa4caa585513996d01e54
Lblackhurst29 <l.blackhurst19@imperial.ac.uk>
2024-03-14 13:13:37 +0000

    adding mkdocs to dependencies

--- src/HMM/misc.py
+++ src/HMM/misc.py
@@ -1,4 +1,3 @@
-from itertools import cycle
 import numpy as np 
 from math import floor, ceil
 import pandas as pd
@@ -86,39 +85,6 @@ def bin_data(data, column, bin_column, function, bin_secs):
     bout_gb.set_index(old_index, inplace =True)
 
     return bout_gb
-
-# def bootstrap(data, n=1000, func=np.mean):
-#     """ 
-#     Generate n bootstrap samples, evaluating `func`
-#     at each resampling. `bootstrap` returns a function,
-#     which can be called to obtain confidence intervals
-#     of interest
-#     params:
-#     @data = numpy array 
-#     @n = number of iterations of the simulation 
-#     @func = function to find average of all simulation outputs
-#     """
-#     simulations = list()
-#     sample_size = len(data)
-#     # xbar_init = np.mean(data)
-    
-#     for c in range(n):
-#         itersample = np.random.choice(data, size=sample_size, replace=True)
-#         simulations.append(func(itersample))
-#     simulations.sort()
-
-#     def ci(p):
-#         """
-#         Return 2-sided symmetric confidence interval specified
-#         by p
-#         """
-#         u_pval = (1+p)/2.
-#         l_pval = (1-u_pval)
-#         l_indx = int(np.floor(n*l_pval))
-#         u_indx = int(np.floor(n*u_pval))
-#         return(simulations[l_indx] , simulations[u_indx])
-
-#     return(ci(0.95))
 
 def _hmm_table(start_prob, trans_prob, emission_prob, state_names, observable_names):
     """ 
