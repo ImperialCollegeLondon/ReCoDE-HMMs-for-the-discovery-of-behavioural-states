445fb37f2b96bcc8e0a817f87bcff7c4c88b9498
Lblackhurst29 <l.blackhurst19@imperial.ac.uk>
2024-04-02 12:59:18 +0100

    fixed ruff errors in functions

--- src/HMM/misc.py
+++ src/HMM/misc.py
@@ -1,10 +1,10 @@
-"""Module for functions to analyse HMM outputs."""
-from typing import Callable
+"""Module for miscellaneous functions used in the notebooks or plotting functions."""
 
-from math import floor, ceil
+from math import floor
+
 import numpy as np
 import pandas as pd
-from pathlib import PurePath
+from hmmlearn.hmm import CategoricalHMM
 from tabulate import tabulate
 
 
@@ -43,37 +43,56 @@ def rle(x: np.array) -> tuple[np.array, np.array, np.array]:
 
         return run_values, run_starts, run_lengths
 
-def create_chain(states, start, length):
 
-    if any([name == start for name in states]) is False:
-        raise TypeError('The starting state name is not in the states list')
+def create_chain(states: list[str], length) -> list:
+    """Creates a sequence according to Markovian principles.
 
-    mat_trans =   [[0.7, 0.05, 0.25],
-                    [0.35, 0.5, 0.15],
-                    [0.05, 0.3, 0.65]]
+    Args:
+        states: A list of len==3, containing the name of the states to be generated
+        length: The length of the generated sequence
+
+    Returns:
+        A python list of len==length of the given states given the transition rates set in the function
+
+    """
+    raise RuntimeError(
+        f"The length of states sould be 3, not {len(states)}"
+    )
+
+    mat_trans = [[0.7, 0.05, 0.25], [0.35, 0.5, 0.15], [0.05, 0.3, 0.65]]
 
     i = 0
 
-    markov_chain = [start]
-    mood = start
+    markov_chain = [states[0]]
+    mood = states[0]
 
     while i != length:
-
         for c, state in enumerate(states):
-
             if mood == state:
-                mood = np.random.choice(states,replace=True,p=mat_trans[c])
+                mood = np.random.choice(states, replace=True, p=mat_trans[c])
                 markov_chain.append(mood)
                 break
 
         i += 1
-    
+
     return markov_chain
 
-def bin_data(
-    data: pd.DataFrame, column: str, bin_column: str, function: str, bin_secs: int
-) -> pd.DataFrame:
-    """A method to bin all data poits to a larger time bin, then summarise a column."""
+
+def bin_data(data: pd.DataFrame, column: str, bin_column: str, function: str, bin_secs: int) -> pd.DataFrame:
+    """A method to bin all data points to a larger time bin, then summarise a column.
+    
+    Args:
+        data: the pandas dataframe with the time series data
+        column: the column the will be aggregated
+        bin_column: the column with the time series data
+        function: the aggregating function to be used on the grouped time series
+        bin_secs: the time (in seconds) that the new dataframe will be binned to
+
+    Returns:
+        A modified pandas data frame with two columns, the new time series and 
+        the aggragated column named f"{column}_{function}"
+
+    """
     index_name = data["id"].iloc[0]
 
     data[bin_column] = data[bin_column].map(lambda t: bin_secs * floor(t / bin_secs))
@@ -89,10 +108,9 @@ def bin_data(
 
     return bout_gb
 
+
 def _hmm_table(start_prob, trans_prob, emission_prob, state_names, observable_names):
-    """ 
-    Prints a formatted table of the probabilities from a hmmlearn MultinomialHMM object
-    """
+    """Prints a formatted table of the probabilities from a hmmlearn MultinomialHMM object."""
     df_s = pd.DataFrame(start_prob)
     df_s = df_s.T
     df_s.columns = state_names
@@ -105,8 +123,19 @@ def _hmm_table(start_prob, trans_prob, emission_prob, state_names, observable_na
     df_e = pd.DataFrame(emission_prob, index=state_names, columns=observable_names)
     print(tabulate(df_e, headers="keys", tablefmt="github") + "\n")
 
-def hmm_display(hmm: Type[hmmlearn.CategoricalHMM], states: list[str], observables: list[str]) -> None:
-    """Prints the transion probabilities for the hidden state and observables."""
+
+def hmm_display(hmm: CategoricalHMM, states: list[str], observables: list[str]) -> None:
+    """Prints the transion probabilities for the hidden state and observables.
+    
+    Args:
+        hmm: the trained hmm whose matrices will be displayed
+        states: a list of the given names of the states for the model
+        observables: a list of the given names of the emissions for the model
+    
+    Returns:
+        None
+
+    """
     _hmm_table(
         start_prob=hmm.startprob_,
         trans_prob=hmm.transmat_,
